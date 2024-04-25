5117029807cee52a001afb140552fd7fdf2b7ff5
Ian Nakazawa <ian.nakazawa@invoisys.com.br>
2024-04-25 20:02:56 -0300

    feat(CreateCategoryUseCaseTest): Iniciando teste de use case

---
+++ application/src/test/java/com/admin/catalog/application/UseCaseTest.java
@@ -0,0 +1,16 @@
+package com.admin.catalog.application;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UseCaseTest {
+
+    @Test
+    public void createUseCase() {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
---
+++ application/src/test/java/com/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
@@ -0,0 +1,45 @@
+package com.admin.catalog.application.category.create;
+
+import com.admin.catalog.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+
+public class CreateCategoryUseCaseTest {
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCommand = new CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
+        Mockito.when(categoryGateway.create(Mockito.any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var useCase = new CreateCategoryUseCase(categoryGateway);
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.getId());
+
+        Mockito.verify(categoryGateway, times(1)).create(argThat(aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.isNull(aCategory.getDeletedAt())
+        ));
+    }
+}
