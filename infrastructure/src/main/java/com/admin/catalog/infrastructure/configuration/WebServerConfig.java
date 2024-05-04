92c0135c18a7ea28236afc559a80066776b7a18c
Ian Nakazawa <ian.nakazawa@invoisys.com.br>
2024-05-04 17:35:13 -0300

    feat(infrastructure): Adicionando spring boot

---
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.admin.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.admin.catalog")
+public class WebServerConfig {
+}
