1df648560fdb06603eb92428917c228208a98eda
Ian Nakazawa <ian.nakazawa@invoisys.com.br>
2024-05-09 20:51:31 -0300

    feat(infrastructure):  Configurando o datasource de dev e test da aplicação

--- infrastructure/src/main/java/com/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/Main.java
@@ -3,10 +3,14 @@ package com.admin.catalog.infrastructure;
 import com.admin.catalog.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
