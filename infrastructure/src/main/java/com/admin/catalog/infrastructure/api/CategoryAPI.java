ad1daec088a8c564fbd46aee6098b01984b4ffd4
Ian Nakazawa <ian.nakazawa@invoisys.com.br>
2024-07-21 17:42:37 -0300

    feat(infrastructure.api): Criando controller e interface com documentação

---
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/api/CategoryAPI.java
@@ -0,0 +1,35 @@
+package com.admin.catalog.infrastructure.api;
+
+import com.admin.catalog.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("categories")
+@Tag(name = "Categories")
+public interface CategoryAPI {
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Created successfully"),
+        @ApiResponse(responseCode = "402", description = "Unprocessable error"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<?> createCategory();
+
+    @GetMapping(produces = "application/json")
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Listed successfully"),
+        @ApiResponse(responseCode = "422", description = "A invalid parameter was received"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    Pagination<?> listCategories(@RequestParam(value = "search", required = false, defaultValue = "") String search, @RequestParam(value = "page", required = false, defaultValue = "0") int page, @RequestParam(value = "perPage", required = false, defaultValue = "10") int perPage, @RequestParam(value = "sort", required = false, defaultValue = "name") String sort, @RequestParam(value = "direction", required = false, defaultValue = "asc") String direction);
+}
---
+++ infrastructure/src/main/java/com/admin/catalog/infrastructure/api/Controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.admin.catalog.infrastructure.api.Controllers;
+
+import com.admin.catalog.domain.pagination.Pagination;
+import com.admin.catalog.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
